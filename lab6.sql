523e4c26864ed7bc64809b0ae06c873e14b0d50b
abatichshev <156772425+abatichshev@users.noreply.github.com>
2024-11-05 17:13:27 +0500

    add lab6

---
+++ lab6.sql
@@ -0,0 +1,52 @@
+CREATE DATABASE lab6;
+
+CREATE TABLE locations(
+    location_id SERIAL PRIMARY KEY,
+    street_address VARCHAR(25),
+    postal_code VARCHAR(12),
+    city VARCHAR(30),
+    state_province VARCHAR(12)
+);
+
+CREATE TABLE departments(
+    department_id SERIAL PRIMARY KEY,
+    department_name VARCHAR(50) UNIQUE,
+    budget INT,
+    location_id INT REFERENCES locations
+);
+
+CREATE TABLE employees(
+    employee_id SERIAL PRIMARY KEY,
+    first_name VARCHAR(50),
+    last_name VARCHAR(50),
+    email VARCHAR(50),
+    phone_number VARCHAR(20),
+    salary INT,
+    department_id INT REFERENCES departments
+);
+
+SELECT e.first_name, e.last_name, d.department_id, d.department_name
+FROM employees e
+JOIN departments d ON e.department_id = d.department_id;
+
+SELECT e.first_name, e.last_name, d.department_id, d.department_name
+FROM employees e
+JOIN departments d ON e.department_id = d.department_id
+WHERE d.department_id = 80 OR d.department_id = 40;
+
+
+SELECT e.first_name, e.last_name, d.department_name, l.city, l.state_province
+FROM employees e
+JOIN departments d ON e.department_id = d.department_id
+JOIN locations l ON d.location_id = l.location_id;
+
+
+SELECT d.department_name
+FROM departments d
+LEFT JOIN employees ON d.department_id = employees.department_id;
+
+
+
+SELECT e.first_name, e.last_name, d.department_id, d.department_name
+FROM employees e
+LEFT JOIN departments d ON e.department_id = d.department_id;
